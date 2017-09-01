681c7401bfede70dc40e622603471da16556368b
pwarren6 <warren46892@gmail.com>
2017-08-31 20:38:54 -0400

    Updated OkhttpClient library module, adding get request and post request.

--- okhttpclient/src/main/java/com/mobileappscompany/training/okhttpclient/OkHTTPClient.java
+++ okhttpclient/src/main/java/com/mobileappscompany/training/okhttpclient/OkHTTPClient.java
@@ -15,6 +15,26 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class OkHTTPClient {
-    OkHTTPClient
+    private OkHttpClient client = new OkHttpClient();
 
+    String doGetRequest(String url) throws IOException{
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    private static final MediaType JSON =
+            MediaType.parse("application/json; charset=utf-8");
+
+    String doPostRequest(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
 }
